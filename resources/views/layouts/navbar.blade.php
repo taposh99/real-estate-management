d1224ac253d5b94ebbaf643a381d7163f1da231f
taposh99 <taposhmd2@gmail.com>
2024-05-26 17:31:46 +0600

    update

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -165,13 +165,13 @@
                         <div class="dropdown-menu dropdown-menu-right profile-notification">
                             <div class="pro-head">
                                 <img src="{{asset('adminAsset')}}/assets/images/user/avatar-1.jpg" class="img-radius" alt="User-Profile-Image">
-                                <span>John Doe</span>
+                                <span>{{ Auth::user()->name }}</span>
                                 <a href="auth-signin.html" class="dud-logout" title="Logout">
                                     <i class="feather icon-log-out"></i>
                                 </a>
                             </div>
                             <ul class="pro-body">
-                                <li><a href="user-profile.html" class="dropdown-item"><i class="feather icon-user"></i> {{ Auth::user()->name }}</a></li>
+                                <li><a href="user-profile.html" class="dropdown-item"><i class="feather icon-user"></i> Profile</a></li>
                                 <li><a href="email_inbox.html" class="dropdown-item"><i class="feather icon-mail"></i> My Messages</a></li>
                                 <li><a href="{{ route('login') }}" class="dropdown-item"><i class="feather icon-lock"></i> Lock Screen</a></li>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
