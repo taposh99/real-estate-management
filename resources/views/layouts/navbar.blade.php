9b86b34374976e114562be4f2fbd38b32da60181
Saniatul Haque Taposh <taposhmd2@gmail.com>
2024-05-27 21:55:03 +0600

    update

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -23,74 +23,7 @@
                         </button>
                     </div>
                 </li>
-                <li class="nav-item">
-                    <div class="dropdown">
-                        <a class="dropdown-toggle h-drop" href="#" data-toggle="dropdown">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu profile-notification ">
-                            <ul class="pro-body">
-                                <li><a href="user-profile.html" class="dropdown-item"><i class="fas fa-circle"></i> Profile</a></li>
-                                <li><a href="email_inbox.html" class="dropdown-item"><i class="fas fa-circle"></i> My Messages</a></li>
-                                <li><a href="#" class="dropdown-item"><i class="fas fa-circle"></i> Lock Screen</a></li>
-                               
-                            </ul>
-                        </div>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <div class="dropdown mega-menu">
-                        <a class="dropdown-toggle h-drop" href="#" data-toggle="dropdown">
-                            Mega
-                        </a>
-                        <div class="dropdown-menu profile-notification ">
-                            <div class="row no-gutters">
-                                <div class="col">
-                                    <h6 class="mega-title">UI Element</h6>
-                                    <ul class="pro-body">
-                                        <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Alert</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Button</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Badges</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Cards</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Modal</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="fas fa-circle"></i> Tabs & pills</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col">
-                                    <h6 class="mega-title">Forms</h6>
-                                    <ul class="pro-body">
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Elements</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Validation</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Masking</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Wizard</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Picker</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-minus"></i> Select</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col">
-                                    <h6 class="mega-title">Application</h6>
-                                    <ul class="pro-body">
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-mail"></i> Email</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-clipboard"></i> Task</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-check-square"></i> To-Do</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-image"></i> Gallery</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-help-circle"></i> Helpdesk</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col">
-                                    <h6 class="mega-title">Extension</h6>
-                                    <ul class="pro-body">
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-file-plus"></i> Editor</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-file-minus"></i> Invoice</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-calendar"></i> Full calendar</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-upload-cloud"></i> File upload</a></li>
-                                        <li><a href="#!" class="dropdown-item"><i class="feather icon-scissors"></i> Image cropper</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+             
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,32 +1,58 @@
 <nav class="pcoded-navbar  ">
-		<div class="navbar-wrapper  ">
-			<div class="navbar-content scroll-div " >
-				
-				<div class="">
-					<div class="main-menu-header">
-						<img class="img-radius" src="{{asset('adminAsset')}}/assets/images/user/avatar-2.jpg" alt="User-Profile-Image">
-						<div class="user-details">
-							<span>Admin</span>
-						</div>
+	<div class="navbar-wrapper  ">
+		<div class="navbar-content scroll-div ">
+
+			<div class="">
+				<div class="main-menu-header">
+					<img class="img-radius" src="{{asset('adminAsset')}}/assets/images/user/avatar-2.jpg" alt="User-Profile-Image">
+					<div class="user-details">
+						<span>Admin</span>
 					</div>
-					
 				</div>
-				
-				<ul class="nav pcoded-inner-navbar ">
-					
-					<li class="nav-item">
-					    <a href="{{route('dashboard')}}" class="nav-link "><span class="pcoded-micon"><i class="feather icon-home"></i></span><span class="pcoded-mtext">Dashboard</span></a>
-					</li>
-					<li class="nav-item pcoded-hasmenu">
-					    <a href="#!" class="nav-link "><span class="pcoded-micon"><i class="feather icon-layout"></i></span><span class="pcoded-mtext">Property Type</span></a>
-					    <ul class="pcoded-submenu">
-					        <li><a href="{{route('property.types.index')}}">Manage Property</a></li>
-					    </ul>
-					</li>
-			
-				</ul>
-			
 
 			</div>
+
+			<ul class="nav pcoded-inner-navbar ">
+
+				<li class="nav-item">
+					<a href="{{route('dashboard')}}" class="nav-link "><span class="pcoded-micon"><i class="feather icon-home"></i></span><span class="pcoded-mtext">Dashboard</span></a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link "><span class="pcoded-micon"><i class="feather icon-layout"></i></span><span class="pcoded-mtext">Manage City</span></a>
+					
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link "><span class="pcoded-micon"><i class="feather icon-layout"></i></span><span class="pcoded-mtext">Manage Location</span></a>
+					
+				</li>
+				<li class="nav-item">
+					<a href="{{route('property.types.index')}}" class="nav-link "><span class="pcoded-micon"><i class="feather icon-layout"></i></span><span class="pcoded-mtext">Property Type</span></a>
+					
+				</li>
+				<li class="nav-item pcoded-hasmenu">
+					<a href="#!" class="nav-link "><span class="pcoded-micon"><i class="feather icon-layout"></i></span><span class="pcoded-mtext">Manage Property</span></a>
+					<ul class="pcoded-submenu">
+						<li><a href="#">All</a></li>
+					</ul>
+				</li>
+
+				<li class="nav-item">
+					<a href="#" class="nav-link "><span class="pcoded-micon"><i class="feather icon-layout"></i></span><span class="pcoded-mtext">Advertisement</span></a>
+					
+				</li>
+
+				<li class="nav-item pcoded-hasmenu">
+					<a href="#!" class="nav-link "><span class="pcoded-micon"><i class="feather icon-layout"></i></span><span class="pcoded-mtext">User Management</span></a>
+					<ul class="pcoded-submenu">
+						<li><a href="#">List Role</a></li>
+						<li><a href="#">Create Role</a></li>
+						<li><a href="#">Create User</a></li>
+					</ul>
+				</li>
+
+			</ul>
+
+
 		</div>
-	</nav>
+	</div>
+</nav>
