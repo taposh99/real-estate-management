6078fadad9576e45269304fded41e4c715e79c53
Saniatul Haque Taposh <taposhmd2@gmail.com>
2024-05-27 22:29:18 +0600

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-  
+
 @section('content')
 <div class="pcoded-main-container">
     <div class="pcoded-content">
@@ -24,65 +24,32 @@
         <div class="row">
             <!-- table card-1 start -->
             <div class="col-md-12 col-xl-4">
-                <div class="card flat-card">
-                    <div class="row-table">
-                        <div class="col-sm-6 card-body br">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <i class="icon feather icon-eye text-c-green mb-1 d-block"></i>
-                                </div>
-                                <div class="col-sm-8 text-md-center">
-                                    <h5>10k</h5>
-                                    <span>Visitors</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 card-body">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <i class="icon feather icon-music text-c-red mb-1 d-block"></i>
-                                </div>
-                                <div class="col-sm-8 text-md-center">
-                                    <h5>100%</h5>
-                                    <span>Volume</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row-table">
-                        <div class="col-sm-6 card-body br">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <i class="icon feather icon-file-text text-c-blue mb-1 d-block"></i>
-                                </div>
-                                <div class="col-sm-8 text-md-center">
-                                    <h5>2000 +</h5>
-                                    <span>Files</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 card-body">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <i class="icon feather icon-mail text-c-yellow mb-1 d-block"></i>
-                                </div>
-                                <div class="col-sm-8 text-md-center">
-                                    <h5>120</h5>
-                                    <span>Mails</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
                 <!-- widget primary card start -->
                 <div class="card flat-card widget-primary-card">
-                    <div class="row-table">
+                    <div class="row-table" style="background-color: #00005C;">
                         <div class="col-sm-3 card-body">
                             <i class="feather icon-star-on"></i>
                         </div>
                         <div class="col-sm-9">
                             <h4>4000 +</h4>
-                            <h6>Ratings Received</h6>
+                            <h6>Total Property</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- widget primary card end -->
+            </div>
+            <div class="col-md-12 col-xl-4">
+
+                <!-- widget primary card start -->
+                <div class="card flat-card widget-primary-card">
+                    <div class="row-table"style="background-color: #0779E4;">
+                        <div class="col-sm-3 card-body">
+                            <i class="feather icon-star-on"></i>
+                        </div>
+                        <div class="col-sm-9">
+                            <h4>4000 +</h4>
+                            <h6>Total Pending Property</h6>
                         </div>
                     </div>
                 </div>
@@ -91,59 +58,124 @@
             <!-- table card-1 end -->
             <!-- table card-2 start -->
             <div class="col-md-12 col-xl-4">
-                <div class="card flat-card">
-                    <div class="row-table">
-                        <div class="col-sm-6 card-body br">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <i class="icon feather icon-share-2 text-c-blue mb-1 d-block"></i>
-                                </div>
-                                <div class="col-sm-8 text-md-center">
-                                    <h5>1000</h5>
-                                    <span>Shares</span>
-                                </div>
-                            </div>
+
+                <!-- widget-success-card start -->
+                <div class="card flat-card widget-purple-card">
+                    <div class="row-table"style="background-color: #035AA6;">
+                        <div class="col-sm-3 card-body">
+                            <i class="fas fa-trophy"></i>
                         </div>
-                        <div class="col-sm-6 card-body">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <i class="icon feather icon-wifi text-c-blue mb-1 d-block"></i>
-                                </div>
-                                <div class="col-sm-8 text-md-center">
-                                    <h5>600</h5>
-                                    <span>Network</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row-table">
-                        <div class="col-sm-6 card-body br">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <i class="icon feather icon-rotate-ccw text-c-blue mb-1 d-block"></i>
-                                </div>
-                                <div class="col-sm-8 text-md-center">
-                                    <h5>3550</h5>
-                                    <span>Returns</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 card-body">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <i class="icon feather icon-shopping-cart text-c-blue mb-1 d-blockz"></i>
-                                </div>
-                                <div class="col-sm-8 text-md-center">
-                                    <h5>100%</h5>
-                                    <span>Order</span>
-                                </div>
-                            </div>
+                        <div class="col-sm-9">
+                            <h4>17</h4>
+                            <h6>Total Approved Property</h6>
                         </div>
                     </div>
                 </div>
+                <!-- widget-success-card end -->
+            </div>
+            <!-- table card-2 end -->
+            <!-- Widget primary-success card start -->
+
+
+        </div>
+        <div class="row">
+            <!-- table card-1 start -->
+            <div class="col-md-12 col-xl-4">
+
+                <!-- widget primary card start -->
+                <div class="card flat-card widget-primary-card">
+                    <div class="row-table"style="background-color: #FF5200;">
+                        <div class="col-sm-3 card-body">
+                            <i class="feather icon-star-on"></i>
+                        </div>
+                        <div class="col-sm-9">
+                            <h4>4000 +</h4>
+                            <h6>Total Cancel Property</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- widget primary card end -->
+            </div>
+            <div class="col-md-12 col-xl-4">
+
+                <!-- widget primary card start -->
+                <div class="card flat-card widget-primary-card">
+                    <div class="row-table"style="background-color: #342EAD;">
+                        <div class="col-sm-3 card-body">
+                            <i class="feather icon-star-on"></i>
+                        </div>
+                        <div class="col-sm-9">
+                            <h4>4000 +</h4>
+                            <h6>Total Property Type</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- widget primary card end -->
+            </div>
+            <!-- table card-1 end -->
+            <!-- table card-2 start -->
+            <div class="col-md-12 col-xl-4">
+
                 <!-- widget-success-card start -->
                 <div class="card flat-card widget-purple-card">
-                    <div class="row-table">
+                    <div class="row-table"style="background-color: #342EAD;">
+                        <div class="col-sm-3 card-body">
+                            <i class="fas fa-trophy"></i>
+                        </div>
+                        <div class="col-sm-9">
+                            <h4>17</h4>
+                            <h6>Total City</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- widget-success-card end -->
+            </div>
+            <!-- table card-2 end -->
+            <!-- Widget primary-success card start -->
+
+
+        </div>
+        <div class="row">
+            <!-- table card-1 start -->
+            <div class="col-md-12 col-xl-4">
+
+                <!-- widget primary card start -->
+                <div class="card flat-card widget-primary-card">
+                    <div class="row-table"style="background-color: #10375C;">
+                        <div class="col-sm-3 card-body">
+                            <i class="feather icon-star-on"></i>
+                        </div>
+                        <div class="col-sm-9">
+                            <h4>4000 +</h4>
+                            <h6>Total Location</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- widget primary card end -->
+            </div>
+            <div class="col-md-12 col-xl-4">
+
+                <!-- widget primary card start -->
+                <div class="card flat-card widget-primary-card">
+                    <div class="row-table"style="background-color: #5C2A9D;">
+                        <div class="col-sm-3 card-body">
+                            <i class="feather icon-star-on"></i>
+                        </div>
+                        <div class="col-sm-9">
+                            <h4>4000 +</h4>
+                            <h6>Total Advertisements</h6>
+                        </div>
+                    </div>
+                </div>
+                <!-- widget primary card end -->
+            </div>
+            <!-- table card-1 end -->
+            <!-- table card-2 start -->
+            <div class="col-md-12 col-xl-4">
+
+                <!-- widget-success-card start -->
+                <div class="card flat-card widget-purple-card">
+                    <div class="row-table"style="background-color: #FF9234;">
                         <div class="col-sm-3 card-body">
                             <i class="fas fa-trophy"></i>
                         </div>
@@ -157,33 +189,8 @@
             </div>
             <!-- table card-2 end -->
             <!-- Widget primary-success card start -->
-            <div class="col-md-12 col-xl-4">
-                <div class="card support-bar overflow-hidden">
-                    <div class="card-body pb-0">
-                        <h2 class="m-0">350</h2>
-                        <span class="text-c-blue">Support Requests</span>
-                        <p class="mb-3 mt-3">Total number of support requests that come in.</p>
-                    </div>
-                    <div id="support-chart"></div>
-                    <div class="card-footer bg-primary text-white">
-                        <div class="row text-center">
-                            <div class="col">
-                                <h4 class="m-0 text-white">10</h4>
-                                <span>Open</span>
-                            </div>
-                            <div class="col">
-                                <h4 class="m-0 text-white">5</h4>
-                                <span>Running</span>
-                            </div>
-                            <div class="col">
-                                <h4 class="m-0 text-white">3</h4>
-                                <span>Solved</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-         
+
+
         </div>
         <!-- [ Main Content ] end -->
     </div>
