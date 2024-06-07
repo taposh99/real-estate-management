8e7367d4bde2b73a6429e8505535517140b86123
Saniatul Haque Taposh <taposhmd2@gmail.com>
2024-06-07 16:10:27 +0600

    font page

--- resources/views/frontend/property/index.blade.php
+++ resources/views/frontend/property/index.blade.php
@@ -3,34 +3,24 @@
 @section('body')
 <section class="intro-single">
     <div class="container">
-      <div class="row">
-        <div class="col-md-12 col-lg-8">
-          <div class="title-single-box">
-            <h1 class="title-single">Our Amazing Properties</h1>
-            <span class="color-text-a">Grid Properties</span>
-          </div>
-        </div>
-        <div class="col-md-12 col-lg-4">
-          <nav aria-label="breadcrumb" class="breadcrumb-box d-flex justify-content-lg-end">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item">
-                <a href="#">Home</a>
-              </li>
-              <li class="breadcrumb-item active" aria-current="page">
-                Properties Grid
-              </li>
-            </ol>
-          </nav>
-        </div>
-      </div>
-    </div>
-  </section><!-- End Intro Single-->
+        <div class="row">
+            <div class="col-md-12 col-lg-8">
+                <div class="title-single-box">
+                    <h1 class="title-single">Our Amazing Properties</h1>
+                </div>
+            </div>
+            <div class="col-md-12 col-lg-4">
 
-  <!-- ======= Property Grid ======= -->
-  <section class="property-grid grid">
+            </div>
+        </div>
+    </div>
+</section><!-- End Intro Single-->
+
+<!-- ======= Property Grid ======= -->
+<section class="property-grid grid">
     <div class="container">
         <div class="row">
-            <div class="col-sm-12">
+            <!-- <div class="col-sm-12">
                 <div class="grid-option">
                     <form method="GET" action="{{ route('property.page') }}">
                         <select name="filter" class="custom-select" onchange="this.form.submit()">
@@ -41,7 +31,7 @@
                         </select>
                     </form>
                 </div>
-            </div>
+            </div> -->
             @forelse ($properties as $data)
             <div class="col-md-4">
                 <div class="card-box-a card-shadow">
@@ -50,15 +40,10 @@
                     </div>
                     <div class="card-overlay">
                         <div class="card-overlay-a-content">
-                            <div class="card-header-a">
-                                <h2 class="card-title-a">
-                                    <a href="#">{{ $data->title }}
-                                        <br /> </a>
-                                </h2>
-                            </div>
+
                             <div class="card-body-a">
                                 <div class="price-box d-flex">
-                                    <span class="price-a">320</span>
+                                    <span class="price-a"> {{ $data->title }}</span>
                                 </div>
                                 <a href="{{ route('property.single', ['id' => $data->id]) }}" class="link-a">Click here to view
                                     <span class="bi bi-chevron-right"></span>
@@ -123,11 +108,10 @@
 </section>
 
 <script>
-  document.querySelector('select[name="filter"]').addEventListener('change', function() {
-      this.form.submit();
-  });
+    document.querySelector('select[name="filter"]').addEventListener('change', function() {
+        this.form.submit();
+    });
 </script>
 
 
 @endsection
-
